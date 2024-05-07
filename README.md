0c5b2be47725ca3ab6c45797cb100159508d71ec
Zoltán Szentgyörgyi <zszentgy@gmail.com>
2024-05-07 09:57:10 +0200

    Readme és main class

---
+++ README.md
@@ -0,0 +1,4 @@
+# Gábor Dénes Egyetem
+## Alkalmazásfejlesztési technológia 2024 tavasz
+### Beadandó feladat
+Készítette: Szentgyörgyi Zoltán
---
+++ src/main/CsigaFutamApp.java
@@ -0,0 +1,6 @@
+package main;
+public class CsigaFutamApp {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
